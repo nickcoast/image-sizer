fba406216094e34d0e1d940493c2d65d8e651666
nickcoast <nick@coastlighting.com>
2016-12-28 17:03:30 -0800

    Fixed rm tempjpg and indent
    Removal of temporary jpg moved outside of if statement so that it always happens.
    Made indentation consistent with 2 spaces.

--- img_resize.sh
+++ img_resize.sh
@@ -19,13 +19,13 @@ else
 fi
 
 if [ $MISSING = true ]; then
-        echo 'Exiting due to previous errors.';
-        exit;
+  echo 'Exiting due to previous errors.';
+exit;
 fi
 
 jpg_list=()
 while IFS= read -d $'\0' -r jpg ; do
-        jpg_list=("${jpg_list[@]}" "$jpg")
+  jpg_list=("${jpg_list[@]}" "$jpg")
 done < <(find * -maxdepth 3 -type f -name "*.jpg" -not -path "zoom/*" -not -path "product/*" -not -path "catalog/*" -not -path "thumb/*" -print0)
 
 # see if any jpegs. maxdepth to look is 3 levels.
@@ -33,8 +33,8 @@ jpgcount=${#jpg_list[@]}
 echo "Found $jpgcount jpegs"
 
 if [ $jpgcount -eq 0 ]; then
-        echo "No '.jpg' files found ('.JPG', '.jpeg', etc., ignored). exiting."
-        exit 0
+  echo "No '.jpg' files found ('.JPG', '.jpeg', etc., ignored). exiting."
+  exit 0
 fi
 
 #set to true to autocrop
@@ -42,43 +42,44 @@ autocrop=false
 
 for i in "${jpg_list[@]}"
 do
-        echo "$i -> zoom, product, catalog, thumb"
-        thisjpg=$(basename $i)
-        dir=$(dirname $i)
-        tempjpg=$dir"/asdfjkl."$thisjpg
-        if [ $autocrop = true ]; then
-                convert $i -trim $tempjpg
-                height=$(identify -format "%h" $tempjpg)
-                width=$(identify -format "%w" $tempjpg)
-                maxdim=$(($height>$width?$height:$width))
-                echo "Maxdim is $maxdim"
-                #if [ $height -ne $width ]; then
-                #
-                #fi
-        fi
-        zoombefore=$(stat --printf='%s' $i)
-        jpegtran -copy none -optimize -outfile $tempjpg $i
-        zoomafter=$(stat --printf='%s' $tempjpg)
-        echo "zoom before: $zoombefore - after: $zoomafter"
-        if [ $zoomafter -lt $zoombefore ]; then
-                echo "It shrank. Using it."
-                #overwrite existing and remove temp
-                                \cp $tempjpg $i
-                rm -f $tempjpg
-        fi
-        zoom='zoom/'$i
-        product='product/'$i
-        catalog='catalog/'$i
-        thumb='thumb/'$i
-        mkdir -p "zoom/$dir"
-#       #NO RESIZE FOR ZOOM
-        cp $i $zoom
-        mkdir -p "product/$dir"
-        convert $i -resize 380x380\> $product
-        mkdir -p "catalog/$dir"
-        convert $i -resize 230x230\> $catalog
-        mkdir -p "thumb/$dir"
-        convert $i -resize 50x50\> $thumb
-        echo "Dir is '$dir'"
-        #echo "Zoom image is '$zoom'"
+  echo "$i -> zoom, product, catalog, thumb"
+  thisjpg=$(basename $i)
+  dir=$(dirname $i)
+  tempjpg=$dir"/asdfjkl."$thisjpg
+  if [ $autocrop = true ]; then
+    convert $i -trim $tempjpg
+    height=$(identify -format "%h" $tempjpg)
+    width=$(identify -format "%w" $tempjpg)
+    maxdim=$(($height>$width?$height:$width))
+    echo "Maxdim is $maxdim"
+    #if [ $height -ne $width ]; then
+    #
+    #fi
+  fi
+  zoombefore=$(stat --printf='%s' $i)
+  jpegtran -copy none -optimize -outfile $tempjpg $i
+  zoomafter=$(stat --printf='%s' $tempjpg)
+  echo "zoom before: $zoombefore - after: $zoomafter"
+  if [ $zoomafter -lt $zoombefore ]; then
+    echo "It shrank. Using it."
+    #overwrite existing and remove temp
+    \cp $tempjpg $i                
+  fi
+  rm -f $tempjpg
+  
+  zoom='zoom/'$i
+  product='product/'$i
+  catalog='catalog/'$i
+  thumb='thumb/'$i
+  mkdir -p "zoom/$dir"
+  #       #NO RESIZE FOR ZOOM
+  cp $i $zoom
+  mkdir -p "product/$dir"
+  convert $i -resize 380x380\> $product
+  mkdir -p "catalog/$dir"
+  convert $i -resize 230x230\> $catalog
+  mkdir -p "thumb/$dir"
+  convert $i -resize 50x50\> $thumb
+  echo "Dir is '$dir'"
+  #echo "Zoom image is '$zoom'"
 done
